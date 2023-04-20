5f231b7a125a2c501ccd27210e467562b37084e5
wlsgh <wlsgh@DESKTOP-KK080RS>
2023-04-20 11:40:56 +0900

    .

---
+++ JSPProj/src/main/java/com/newlecture/web/Calc2.java
@@ -0,0 +1,30 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Calc2")
+public class Calc2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String v_ = request.getParameter("v");
+		String op = request.getParameter("operator");
+		int v = 100;
+		if(!v_.equals("")) v = Integer.parseInt(v_);
+		
+		PrintWriter out = response.getWriter();
+		
+	}
+
+}
