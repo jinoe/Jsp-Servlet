c357f88590e1a2c84dc949b52cce44941a530fa6
wlsgh <wlsgh@DESKTOP-KK080RS>
2023-04-25 19:22:51 +0900

    get과 post의 차이

--- JSPProj/src/main/java/com/newlecture/web/CalcPage.java
+++ JSPProj/src/main/java/com/newlecture/web/CalcPage.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/calcpage")
 public class CalcPage extends HttpServlet {
-	private static final long serialVersionUID = 1L;
        
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
